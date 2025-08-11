5b662570e962b5f5b734452f714dc7fe1c424525
Andraz Gnidovec <andraz.gnidovec@fmf.uni-lj.si>
2025-08-11 16:58:50 +0200

    Handling of dipoles for inserted particles.

--- src/REACTION/fix_bond_react.h
+++ src/REACTION/fix_bond_react.h
@@ -265,7 +265,7 @@ class FixBondReact : public Fix {
     tagint tag, molecule;
     int type, mask;
     imageint image;
-    double rmass, x[3], v[3];
+    double rmass, x[3], v[3], mu[3];
   };
   std::vector<AddAtom> addatoms;
 
